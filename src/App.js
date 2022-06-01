df88aed857f145950d69f173e11dc0fa6a0132d5
joeephwild <josephomotade0@gmail.com>
2022-06-01 12:43:57 -0700

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Details from './components/Details';
+import Hero from './components/Hero';
+import Episodes from './components/Episode';
+import Videos from './components/Videos';
+import Info from './components/Info'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      <Hero/>
+      <Details/>
+      <Episodes/>
+      <Videos/>
+      <Info/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/utility/colors.js
@@ -0,0 +1,4 @@
+export const pink = "#e54588";
+export const blue = "#85afd5";
+export const backgroundColor = "pink";
+export const fontLightColor = "#c0c0c0";
